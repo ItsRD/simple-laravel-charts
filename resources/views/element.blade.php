27798358076c5f404bce4de3677f93ade4632f6a
Rick van der Burg <rick@pixcero.nl>
2024-04-22 13:14:41 +0200

    Unique ids for multiple charts + removed facade

--- resources/views/element.blade.php
+++ resources/views/element.blade.php
@@ -1,3 +1,3 @@
-<div class="chart-container" style="height: 300px; width: 100%;">
+<div class="chart-container" style="height: 400px; width: 100%;">
     <canvas id="{{ $id }}"></canvas>
 </div>
--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -1,5 +1,5 @@
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script>
-    var ctx = document.getElementById('{{ $charts->getId() }}').getContext('2d');
-    new Chart(ctx, @json($charts->getChartObject()));
+    var {{ Str::camel($charts->getId()) }} = document.getElementById('{{ $charts->getId() }}').getContext('2d');
+    new Chart({{ Str::camel($charts->getId()) }}, @json($charts->getChartObject()));
 </script>
